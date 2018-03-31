eeb3ad9d34de6f033541089f14f0800b18a5e6c4
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-03-31 12:08:37 +0530

    Basic code

--- main.m
+++ main.m
@@ -1,29 +1,32 @@
 # Throughput analysis for Selective repeat ARQ channel with time-out
 
-ACK = 1;                   # Positive Acknowledgement
-NAK = -1;                  # Negative Acknowledgement
+ACK = 1;                                      # Positive Acknowledgement
+NAK = -1;                                     # Negative Acknowledgement
 
 n = input("Enter the size of the sender and receiver: ");
 
-sender = zeros(n);         # The sender and receiver of size 'n'
+sender = zeros(n);                            # The sender and receiver of size 'n'
 receiver = zeros(n);
 
-for(i = 1:n)
+for i = 1:n
   d = input("Enter the data bits to be sent: ");
-  sender(i) = d;           # Input the data to be sent 0's or 1's
+  sender(i) = d;                              # Input the data to be sent 0's or 1's
 end
-
-while(1)
-  {
-    int i = n-(n-1);       # Block count numbers
-    int ack;
-    if(ack==ACK)           # Do not retransmit it the Acknowledgement for the particular block is Positive
-    {
-
-      n--;
-    }
-    else if(ack = NAK)     # Go on with the re transmission if the Acknowledgement is Negative
-    {
-
-    }
-  }
+i=1;
+disp(sender);
+while n>0
+    receiver(i)=mod(randi(5),2)               # Using random function to mimic errors in transmission
+    if receiver(i)==sender(i)                 # If what is recieved is correct,
+      ack = 1;                                # Set Acknowledgement as 1,
+    else
+      ack = -1;                               # Else provide negative Acknowledgement
+    end
+    if ack==ACK                               # Move to next transmission if recieved  is correct.
+      disp(ack);
+      n--
+      i++
+    else if ack == NAK                        # Go on with the re transmission if the Acknowledgement is Negative
+      disp(ack);
+    end
+end
+end
